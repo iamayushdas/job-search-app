7ba09a12473c86edcfcddac9322ed4cc222f57f2
iamayushdas <ayush24das@gmail.com>
2024-01-07 21:20:12 +0530

    feat:app completed

---
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, Text, Image, Pressable } from "react-native";
+
+import styles from "./nearbyjobcard.style";
+import { checkImgUrl } from "../../../../utils";
+const NearbyJobCard = ({ job, handleNavigate }) => {
+  return (
+    <Pressable style={styles.container} onPress={handleNavigate}>
+      <Pressable style={styles.logoContainer}>
+        <Image
+          source={{
+            url: checkImgUrl(job?.employer_logo)
+              ? job?.employer_logo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
+          resizeMode="contain"
+          style={styles.logoImage}
+        />
+      </Pressable>
+      <View style={styles.textContainer}>
+        <Text style={styles.jobName} numberOfLines={1}>
+          {job.job_title}
+        </Text>
+        <Text style={styles.jobType}>{job.job_employment_type}</Text>
+      </View>
+    </Pressable>
+  );
+};
+
+export default NearbyJobCard;
---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../assets/images/AD_profile.png";
+
+export default {
+  profile,
+};
